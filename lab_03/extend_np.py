c2899de4fabd67e3d64524be30bf1399610c0f21
Magistrat <nisraton@gmail.com>
2023-12-17 21:20:46 +0300

    [feature] done numpy

--- lab_03/extend_np.py
+++ lab_03/extend_np.py
@@ -23,9 +23,9 @@ print('Создание случайных матриц через empty и full
 
 ar_change_type = np.arange(10)
 ar_change_type.dtype = np.float64
-print('Изменение типа', ar_change_type)
+print('Изменение типа на float64', ar_change_type)
 ar_change_type.dtype = np.int64
-print('Изменение типа', ar_change_type, **ARG_)
+print('Возврат на int64', ar_change_type, **ARG_)
 
 ar_for_max_min = np.arange(5, 10)
 max_ = np.max(ar_for_max_min)
@@ -33,3 +33,29 @@ min_ = np.min(ar_for_max_min)
 mean_ = np.mean(ar_for_max_min)
 print('Максимум, Минимум, средне арифметическое', max_, min_, mean_, **ARG_)
 
+random_ar = np.random.rand(5)
+print('Массив из 5 рандомных элементов (от 0 до 1)', random_ar, **ARG_)
+
+random_randint = np.random.randint(50, 60, (3, 2))
+print('Матрица с рандомными элементами от 50 до 60', random_randint, **ARG_)
+
+ar_shuffle = np.arange(10)
+np.random.shuffle(ar_shuffle)
+print('Перетасовка массива от 0 до 10', ar_shuffle, **ARG_)
+
+random_for_set = np.random.randint(5, 10, 10)
+print(f'Создание множества из {random_for_set}')
+set_1 = np.unique(random_for_set, return_counts=True)
+print(f'Количество повторений {set_1}')
+set_2 = np.unique(random_for_set, return_index=True)
+print(f'Индекс первого вхождения {set_2}')
+set_3 = np.unique(random_for_set, return_inverse=True)
+print(f'Массив для восстановления {set_3}', **ARG_)
+
+ar1 = np.arange(10)
+print(f'Массив {ar1}')
+print(f'Тип элементов {ar1.dtype}')
+print(f'Длина {ar1.size}')
+print(f'Количество осей {ar1.ndim}')
+print(f'Узнать размерность {ar1.shape}')
+print(f'Задать размерность {ar1.reshape((2, 5)).shape}')
